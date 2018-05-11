43402357ed4ab1aaad42a8e05969b2e7e897a9db
ZipCPU <dgisselq@ieee.org>
2018-05-11 11:37:16 -0400

    Added a random access capability to the scope class

--- sw/scopecls.h
+++ sw/scopecls.h
@@ -220,6 +220,12 @@ public:
 	// structures, for reference when writing the VCD file.
 		void	register_trace(const char *varname,
 				unsigned nbits, unsigned shift);
+
+	unsigned operator[](unsigned addr) {
+		if ((m_data)&&(m_scoplen > 0))
+			return m_data[(addr)&(m_scoplen-1)];
+		return 0;
+	}
 };
 
 #endif	// SCOPECLS_H
